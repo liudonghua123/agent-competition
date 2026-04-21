94bccffac52f0ba8a817e57b0fc25c1e9eaaa0ce
liudonghua <liudonghua123@gmail.com>
2026-04-21 09:37:56 +0800

    fix Error: EMFILE: too many open files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -25,6 +25,9 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true
       }
-    }
+    },
+    watch: {
+      ignored: ['**/node_modules/**', '**/dist/**'],
+    },
   }
 })
